c1600bb3e0f090cb45d4c05dcde7aa5a262316aa
Guy Barzily <85988766+GuyBarzily@users.noreply.github.com>
2022-05-30 21:04:43 +0300

    added signup and signin arrays

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,20 @@
+import { useState } from "react";
+
 export enum AppPages {
     Home = "/",
     Expenses = "/exp",
     SignUp = "/signup",
+
+
     
 }
+
+
+const SignInData: Array<FormData> = [];
+export {SignInData};
+
+const SignUpData: Array<FormData> = [];
+export {SignUpData};
+
+// const [didSignedUp, setDidSignedUp] = useState(false);
+// export {didSignedUp}
