1025d7f1b70161ea5bfeaa36670ef3f92a4cf572
Guy Barzily <85988766+GuyBarzily@users.noreply.github.com>
2022-05-31 22:05:36 +0300

    added expensses table

--- src/pages/Expenses.tsx
+++ src/pages/Expenses.tsx
@@ -1,12 +1,19 @@
 import { Typography } from "@mui/material";
 import React from "react";
+import Table from "../components/Table";
 
 const Expenses = () => {
   return (
     <div>
-      <Typography variant="h2" color="primary" textAlign="center">
-        Expenses
-      </Typography>
+      <div>
+        <Typography variant="h2" color="primary" textAlign="center">
+          Expenses
+        </Typography>
+      </div>
+      <br />
+      <div>
+        <Table />
+      </div>
     </div>
   );
 };
