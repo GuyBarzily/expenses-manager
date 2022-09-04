51c269cdf6e4349756d8d34ed787cb26eea9abf0
Guy Barzily <85988766+GuyBarzily@users.noreply.github.com>
2022-09-04 15:56:23 +0300

    moved get all from home

--- frondend/src/pages/Home.tsx
+++ frondend/src/pages/Home.tsx
@@ -13,61 +13,6 @@ import { getAllFin } from "../axios";
 const Home = () => {
   const financialState = useContext(FinancialStateContext);
   const setFinancialState = useContext(SetFinancialStateContext);
-  useEffect(() => {
-    console.log("use Effect");
-    getFinancial();
-  }, []);
-  const appendIncomes = (incomeData: object, expenseData: object) => {
-    // console.log(incomeData);
-    // console.log(expenseData);
-    const tmp = JSON.stringify(incomeData);
-    const data = JSON.parse(tmp);
-    const income: FinancialItem[] = [];
-    for (let i = 0; i < data.length; i++) {
-      const num: number = +data[i].amountValue.replaceAll(",", "");
-      const tmp: FinancialItem = {
-        descriptionValue: data[i].descriptionValue,
-        date: data[i].dateValue,
-        type: data[i].incomeTypeValue,
-        value: num,
-        currencySign: data[i].currencyValue,
-      };
-      income.push(tmp);
-    }
-
-    const tmp1 = JSON.stringify(expenseData);
-    const data1 = JSON.parse(tmp1);
-    const expense: FinancialItem[] = [];
-    for (let i = 0; i < data1.length; i++) {
-      const num: number = +data1[i].amountValue.replaceAll(",", "");
-      const tmp: FinancialItem = {
-        descriptionValue: data1[i].descriptionValue,
-        date: data1[i].dateValue,
-        type: data1[i].expensTypeValue,
-        value: num,
-        currencySign: data1[i].currencyValue,
-      };
-      expense.push(tmp);
-    }
-    if (!setFinancialState)
-      throw new Error("setFinancialState was not initialized");
-    setFinancialState({
-      ...financialState,
-      income: income,
-      expenses: expense,
-    });
-  };
-
-  async function getFinancial() {
-    // console.log("inside get Financial");
-    const res = await getAllFin();
-    // console.log(res);
-    const income = res.data.income;
-    const expense = res.data.expenses;
-    appendIncomes(income, expense);
-    //  appendExpense(expense);
-  }
-
   const handleAddExpenses = (financialItem: FinancialItem) => {
     const newExpenses = [...financialState.expenses, financialItem];
     if (!setFinancialState)
